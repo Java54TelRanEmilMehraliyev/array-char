95906eb3fad97d2ca5e809683565744811d5860e
user.emial <emil.mehraliyev003@gmail.com>
2024-03-07 21:05:48 +0200

    ClassWork of Array-Char methods

---
+++ src/backend/array/ArrayChar.java
@@ -0,0 +1,31 @@
+package backend.array;
+
+public class ArrayChar {
+   private char[] array;
+   public ArrayChar(char[] array) {
+	  this.array = array;
+   }
+   public int compareTo(ArrayChar another) {
+	   int length = Math.min(this.array.length, another.array.length); 
+	   int index = 0;
+	   while(index < length && this.array[index] == another.array[index]) {
+		 index++; 
+	   }
+	   return index == length ? this.array.length - another.array.length
+			   : this.array[index] - another.array[index];
+   }
+   public int indexOf(char character) {
+	  int index = 0;
+	  while(index < this.array.length && array[index] != character) {
+		 index++; 
+	  }
+	   return index == array.length ? -1 : index;
+   }
+   public int lastIndexOf(char character) {
+       int index = array.length-1;
+       while(index > -1 && this.array[index] != character) {
+    	   index--;
+       }
+	   return index;
+   }
+}
---
+++ src/backend/array/test/ArrayCharTest.java
@@ -0,0 +1,42 @@
+package backend.array.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import backend.array.ArrayChar;
+
+class ArrayCharTest {
+
+	@Test
+	void compareToTest() {
+		char[] ar1 = {'1', '2', '3'};
+		char[] ar2 = {'9'};
+		char[] ar3 = {'1', '2', '3'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		ArrayChar arrayChar2 = new ArrayChar(ar2);
+		ArrayChar arrayChar3 = new ArrayChar(ar3);
+		assertEquals(8, arrayChar2.compareTo(arrayChar1));
+		assertEquals(-8, arrayChar1.compareTo(arrayChar2));
+		assertEquals(1, new ArrayChar(new char[] {'a','a'})
+				.compareTo(new ArrayChar(new char[] {'a'})));
+		assertEquals(-1, new ArrayChar(new char[] {'a'})
+				.compareTo(new ArrayChar(new char[] {'a', 'a'})));
+		assertEquals(0, arrayChar1.compareTo(arrayChar3));
+	}
+	@Test
+	void indexOfTest() {
+		char[] ar1 = {'1', '2', '3','2'};
+        ArrayChar arrayChar1 = new ArrayChar(ar1);
+        assertEquals(1,arrayChar1.indexOf('2'));
+        assertEquals(-1,arrayChar1.indexOf('9'));
+	}
+	@Test
+	void lastIndexOfTest() {
+		char[] ar1 = {'1','2','3','2'};
+		ArrayChar arrayChar1 = new ArrayChar(ar1);
+		assertEquals(3,arrayChar1.lastIndexOf('2'));
+		assertEquals(-1,arrayChar1.lastIndexOf('5'));
+	}
+
+}
